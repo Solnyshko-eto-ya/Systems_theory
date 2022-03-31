5b6c937d4543c4c32baeab52c1a3b3e13778d95b
Drew Kozlov <andreigbk@gmail.com>
2022-03-31 18:23:34 +0300

    Andryfaciya

--- Monty_Hall/main.py
+++ Monty_Hall/main.py
@@ -1,44 +1,44 @@
 import random
 
-# к - козел, а - автомобиль
-choices = ['к', 'к', 'а']
-# число опытов
-N = 10000
 
+class MontyHall:
+    def __init__(self):
+        self.choices = ['к', 'к', 'а']
+        self.iter = 10000
 
-class choice_without_change:
-    def without_change(choices):  # в начале каждой попытки случайно перемешаем массив
-        random.shuffle(choices)
-        return choices[random.randrange(len(choices))]
+    @staticmethod
+    def counter(func):
+        # проверяем вариант с неизменным выбором
+        def wrapper(self):
+            win_count = 0
+            for _ in range(self.iter):
+                result = func(self)
+                if result == 'а':
+                    win_count += 1
+            # вероятность выиграть - частота выигранных опытов
+            print(win_count / self.iter)
 
-    # проверяем вариант с неизменным выбором
-    win_count = 0
-    for _ in range(N):
-        result = without_change(choices)
-        if result == 'а':
-            win_count += 1
-    # вероятность выиграть - частота выигранных опытов
-    print(win_count / N)
+        return wrapper
 
+    @counter
+    def without_change(self):  # в начале каждой попытки случайно перемешаем массив
+        random.shuffle(self.choices)
+        return self.choices[random.randrange(len(self.choices))]
 
-class choice_with_change:
-    def with_change(choices):   # в начале каждой попытки случайно перемешаем массив
-        random.shuffle(choices)
-        first_choice = random.randrange(len(choices))  # первый выбор
-        for i in range(len(choices)):  # ведущий открывает дверь с козлом
-            if i != first_choice and choices[i] == 'к':
+    @counter
+    def with_change(self):  # в начале каждой попытки случайно перемешаем массив
+        random.shuffle(self.choices)
+        first_choice = random.randrange(len(self.choices))  # первый выбор
+        for i in range(len(self.choices)):  # ведущий открывает дверь с козлом
+            if i != first_choice and self.choices[i] == 'к':
                 host_choice = i
                 break
-        for i in range(len(choices)):  # делаем второй выбор, меняя первое решение
+        for i in range(len(self.choices)):  # делаем второй выбор, меняя первое решение
             if i != first_choice and i != host_choice:
-                return choices[i]
+                return self.choices[i]
 
 
-    # проверяем вариант с изменением выбора
-    win_count = 0
-    for _ in range(N):
-        result = with_change(choices)
-        if result == 'а':
-            win_count += 1
-    # вероятность выиграть - частота выигранных опытов
-    print(win_count/N)
+if __name__ == "__main__":
+    counter = MontyHall()
+    counter.with_change()
+    counter.without_change()
